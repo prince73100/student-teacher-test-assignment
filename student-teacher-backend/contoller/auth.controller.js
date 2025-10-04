359ceaf74dbbde8552087e79eaec827582b57ff3
aadi-prince <aadiprince424@gmail.com>
2025-10-04 10:11:15 +0530

    fix token bug

--- student-teacher-backend/contoller/auth.controller.js
+++ student-teacher-backend/contoller/auth.controller.js
@@ -6,8 +6,8 @@ import jwt from 'jsonwebtoken';
 const sendTokenAsCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,                      
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: 'strict',                 
+    secure: true, 
+    sameSite: 'none',                 
     maxAge: 7 * 24 * 60 * 60 * 1000     
   });
 };
